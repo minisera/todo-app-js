2a3b2205b71821529a8ae23e5274c82090ddacbb
koki otaka <67394974+minisera@users.noreply.github.com>
2021-09-04 13:16:18 +0900

    feat/追加押下した時にTODOが追加される

--- src/index.js
+++ src/index.js
@@ -5,12 +5,33 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  // 未完了リスト追加
+  // divタグ生成
   const div = document.createElement("div");
   div.className = "list-row";
+
+  // liタグ生成
   const li = document.createElement("li");
   li.innerText = inputText;
+
+  // button（完了）タグ生成
+  const completeBotton = document.createElement("button");
+  completeBotton.innerText = "完了";
+  completeBotton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button（削除）タグ生成
+  const deleteBotton = document.createElement("button");
+  deleteBotton.innerText = "削除";
+  deleteBotton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // divタグに各タグを挿入
   div.appendChild(li);
+  div.appendChild(completeBotton);
+  div.appendChild(deleteBotton);
+
   document.getElementById("incomplete-list").appendChild(div);
 };
 
